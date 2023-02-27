094d84ef79101807392e62f9874e9226c61f86ec
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-02-27 02:12:52 +0000

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+const apiKey ="yxjq95gxuj9m";
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-        <h1>Medical Chat</h1>
+    <div className="app__wrapper" >
+        <Chat client={client} theme="team light">
+        <ChannelListContainer/>
+        <ChannelContainer/>
+
+        </Chat>
     </div>
   )
 }
