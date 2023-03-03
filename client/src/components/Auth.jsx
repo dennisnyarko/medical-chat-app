4410aa9bb5fb22727e663229d98188aae30034e3
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-03-03 23:58:36 +0000

    update

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,6 +4,8 @@ import axios from 'axios'
 
 import signinImage from '../assets/signup.jpg';
 
+const cookies = new Cookies();
+
 const initialState = {
     fullName: '',
     username: '',
@@ -22,15 +24,33 @@ const Auth = () => {
         
     }
 
-    const switchMode = async () => {
-        setIsSignup((prevIsSignup) => !prevIsSignup)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        const { fullName, username, password, phoneNumber, avatarURL } = form;
+
+        const URL = 'http://localhost:9000/auth';
+
+        const { data: { token, userId, hashedPassword} } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+            username, password, fullName, phoneNumber, avatarURL,
+        });
+
+        cookies.set('token', token);
+        cookies.set('username', username);
+        cookies.set('fullName', fullName);
+        cookies.set('userId', userId);
+
+        if(isSignup) {
+        cookies.set('phoneNumber', phoneNumber);
+        cookies.set('avatarURL', avatarURL);
+        cookies.set('hashedPassword', hashedPassword);
+        
+            
+        }
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        
-
+    const switchMode = async () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup)
     }
 
 
