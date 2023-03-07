19ce70118f73499342ed0692d592ef1a50f7f410
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-03-07 23:50:34 +0000

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
@@ -26,14 +26,29 @@ if(authToken) {
 }
 
 const App = () => {
+    const [createType, setCreateType] = useState('');
+    const [isCreating, setIsCreating] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+
     if(!authToken) return <Auth />
 
 
   return (
     <div className="app__wrapper">
         <Chat client={client} theme="team light">
-        <ChannelListContainer/>
-        <ChannelContainer/>
+        <ChannelListContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+        />
+        <ChannelContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          createType={createType}
+        />
 
         </Chat>
     </div>
--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -4,6 +4,21 @@ import { Channel, useChatContext } from 'stream-chat-react';
 import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from './';
 
 const ChannelContainer = () => {
+  const { channel } = useChatContext();
+
+  if(isCreating) {
+    return (
+
+    )
+  }
+
+  if(isEditing) {
+    return (
+      
+    )
+  }
+
+
   return (
     <div>ChannelContainer</div>
   )
