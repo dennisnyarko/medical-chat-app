05e61aee4d602b93c69d4fd39d6ee844a6688e55
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-03-09 21:54:12 +0000

    update

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -24,14 +24,14 @@ const ChannelContainer = ({isCreating, setIsCreating, isEditing, setIsEditing, c
     )
   }
 
-  const EmptyState = () => {
+  const EmptyState = () => (
     <div className="channel-empty__container">
     <p className="channel-empty__first">This is the beginning of your chat history</p>
     <p className="channel-empty__second">Send messages, attachments, links, emojis and more!</p>
 
     </div>
     
-  }
+  )
 
   return (
     <div className="channel__container">
