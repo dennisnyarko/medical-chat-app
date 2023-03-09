5895c62a36de3bb1bc4f474507e31d4758f14064
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-03-09 16:24:07 +0000

    update

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,8 +3,8 @@ import { ChannelList, useChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
-import HospitalIcon from "../assets/hospital.png"
-import LogoutIcon from "../assets/logout.png"
+import HospitalIcon from "../assets/hospital.png";
+import LogoutIcon from "../assets/logout.png";
 
 const cookies = new Cookies();
 
